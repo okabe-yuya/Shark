5f885297b8f485413ba18332fee67809a121bf23
yuya okabe <takamizawa46@gmail.com>
2019-06-02 22:55:52 +0900

    develop column remove, and column list, and column exist check

--- lib/column.ex
+++ lib/column.ex
@@ -1,7 +1,6 @@
 defmodule SharkColumn do
   def add_header(cdl, column_name, init_val, is_head \\ false) do
-    new_header = _add_header(cdl, column_name, init_val, is_head)
-    new_header
+    _add_header(cdl, column_name, init_val, is_head)
   end
 
   defp _add_header(cdl, column_name, init_val, is_head) do
@@ -14,5 +13,26 @@ defmodule SharkColumn do
       |> Stream.map(&(List.insert_at(&1, index_num, init_val)))
       |> Stream.drop(1)
     Stream.concat([header, body])
+  end
+
+  def remove_header(cdl, column_name) do
+    key_index_num =
+      cdl
+      |> res_cdl_header()
+      |> Enum.find_index(fn val -> val == column_name end)
+    Stream.map(cdl, &(List.delete_at(&1, key_index_num)))
+  end
+
+  def is_exist_name_in_header(cdl, column_name) do
+    cdl
+    |> res_cdl_header()
+    |> Enum.member?(column_name)
+  end
+
+  def res_cdl_header(cdl) do
+    cdl
+    |> Stream.take(1)
+    |> Enum.to_list()
+    |> List.first()
   end
 end
